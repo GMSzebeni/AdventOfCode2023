472a87a6ff8a763d12bab10069df0d4e816df90d
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2023-12-01 15:33:08 +0100

    day1 first task has been completed

--- src/main/java/org/example/Aoc01122023.java
+++ src/main/java/org/example/Aoc01122023.java
@@ -1,9 +1,42 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Aoc01122023 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(calibrate("files/01122023data.txt"));
     }
 
-    
+    public static int getNumbersFromLines(String line) {
+        int number = 0;
+        Pattern patternFirst = Pattern.compile("\\d");
+        Pattern patternLast = Pattern.compile("\\d(?!.*\\d)");
+        Matcher matcherFirst = patternFirst.matcher(line);
+        Matcher matcherLast = patternLast.matcher(line);
+        if (matcherFirst.find() && matcherLast.find()) {
+            number += Integer.parseInt(matcherFirst.group()) * 10;
+            number += Integer.parseInt(matcherLast.group());
+        }
+        return number;
+    }
+
+    public static int calibrate(String fileName) {
+        int result = 0;
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                result += getNumbersFromLines(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File cannot be found.");
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
