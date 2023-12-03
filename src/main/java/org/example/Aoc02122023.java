9cc335498a6134fa47efb6abc7df351fa4269a76
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2023-12-03 06:55:19 +0100

    day2 first task has been completed

---
+++ src/main/java/org/example/Aoc02122023.java
@@ -0,0 +1,59 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Aoc02122023 {
+    public static void main(String[] args) {
+        System.out.println(allPossibleGames(new int[]{12, 13, 14}, "files/02122023data.txt"));
+    }
+
+    public static int allPossibleGames(int[] maxAmountOfColors, String fileName) {
+        int result = 0;
+        try {
+            File file = new File(fileName);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                result += findPossibleGame(maxAmountOfColors, scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File cannot be found.");
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static int getId(String line) {
+        Matcher matcher = Pattern.compile("\\d+").matcher(line);
+        int index = 0;
+        if (matcher.find()) {
+            index = Integer.parseInt(matcher.group());
+        }
+        return index;
+    }
+
+    public static int findPossibleGame(int[] maxAmountOfColors, String line) {
+        List<String> sets = new ArrayList<>(List.of(line.substring(line.indexOf(":") + 2).split("; |, ")));
+
+        for (String set : sets) {
+            String[] colors = new String[]{"red", "green", "blue"};
+            for (int j = 0; j < colors.length; j++) {
+                if (set.contains(colors[j])) {
+                    String[] number = set.split("\\s", 2);
+                    int amount = Integer.parseInt(number[0]);
+                    if (amount > maxAmountOfColors[j]) {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        return getId(line);
+    }
+}
