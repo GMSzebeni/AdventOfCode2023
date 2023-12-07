48d157c107b8702604422c75336b7124108c8753
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2023-12-07 11:02:29 +0100

    day4 second task has been completed

--- src/main/java/org/example/Aoc04122023.java
+++ src/main/java/org/example/Aoc04122023.java
@@ -2,9 +2,10 @@ package org.example;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,23 +14,30 @@ public class Aoc04122023 {
         System.out.println(sum(35, "files/04122023data.txt"));
     }
 
-    public static int sum(int totalAmountOfNumbersInALine, String fileName) {
-        int result = 0;
+    public static String sum(int totalAmountOfNumbersInALine, String fileName) {
+        int resultOfFirstTask = 0;
+        int resultOfSecondTask = 0;
         try {
             File file = new File(fileName);
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
-                result += winnings(totalAmountOfNumbersInALine, scanner.nextLine().substring(10));
+                String line = scanner.nextLine();
+                int winnings = winningsForFirstTask(totalAmountOfNumbersInALine, line.substring(10));
+                resultOfFirstTask += winnings;
             }
             scanner.close();
+            resultOfSecondTask = resultOfSecondTask(totalAmountOfNumbersInALine, Files.readAllLines(Path.of("files/04122023data.txt")));
         } catch (FileNotFoundException e) {
             System.out.println("File cannot be found.");
             e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("File cannot be read.");
+            e.printStackTrace();
         }
-        return result;
+        return "Result of first task: " + resultOfFirstTask + " & result of second task: " + resultOfSecondTask;
     }
 
-    private static int winnings(int totalAmountOfNumbersInALine, String line) {
+    private static int winningsForFirstTask(int totalAmountOfNumbersInALine, String line) {
         Set<Integer> numbers = new HashSet<>();
         Matcher matcher = Pattern.compile("\\d+").matcher(line);
 
@@ -41,5 +49,36 @@ public class Aoc04122023 {
 
         if (winners == 0) return 0;
         return (int) Math.pow(2, (double) winners - 1);
+    }
+
+    private static int resultOfSecondTask(int totalAmountOfNumbersInALine, List<String> lines) {
+        List<List<Integer>> amountsAndWinnings = new ArrayList<>();
+        for (String line : lines) {
+            amountsAndWinnings.add(new ArrayList<>(List.of(1, winningsForSecondTask(totalAmountOfNumbersInALine, line.substring(10)))));
+        }
+
+        int result = 0;
+
+        for (int i = 1; i < amountsAndWinnings.size(); i++) {
+            int winnersInATicket = amountsAndWinnings.get(i - 1).get(1);
+            int amountOfTicketsLeft = amountsAndWinnings.size() - (i - 1);
+            for (int j = i; j < i + winnersInATicket && winnersInATicket < amountOfTicketsLeft; j++) {
+                amountsAndWinnings.get(j).set(0, amountsAndWinnings.get(j).get(0) + amountsAndWinnings.get(i - 1).get(0));
+            }
+            result += amountsAndWinnings.get(i - 1).get(0);
+        }
+
+        return result + amountsAndWinnings.get(amountsAndWinnings.size() - 1).get(0);
+    }
+
+    private static int winningsForSecondTask(int totalAmountOfNumbersInALine, String line) {
+        Set<Integer> numbers = new HashSet<>();
+        Matcher matcher = Pattern.compile("\\d+").matcher(line);
+
+        while (matcher.find()) {
+            numbers.add(Integer.parseInt(matcher.group()));
+        }
+
+        return totalAmountOfNumbersInALine - numbers.size();
     }
 }
